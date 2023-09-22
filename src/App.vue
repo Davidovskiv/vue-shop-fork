48e5a298d856310c78ed4a02f6880cd02d59384c
Bartek Lazarowski <passat353@interia.pl>
2023-09-22 04:42:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="conteiner">
+        <router-view></router-view>
+    </div>
+</template>
+
+<style>
+.conteiner {
+    min-width: 100vw;
+    min-height: 100vh;
+    background-color: rgb(57, 57, 57);
+}
+</style>
---
+++ src/pages/Products.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navigation from "../components/Navigation.vue"
+import ProductList from "../components/ProductsList.vue"
+</script>
+
+<template>
+    <Navigation />
+    <ProductList />
+</template>
+
